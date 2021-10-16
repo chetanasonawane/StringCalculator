2774a671ab82f1454af5523ea754c768f64422c0
Chetana Sonawane <chetanasonawane95@gmail.com>
2021-10-16 18:11:36 +0530

    Third commit

--- StringCalculator/src/StringCalculator.java
+++ StringCalculator/src/StringCalculator.java
@@ -1,16 +1,16 @@
 
 public class StringCalculator {
-	public static final int add(final String numbers) { // Changed void to int
+	public static int add(final String numbers) {
+	    int returnValue = 0;
 	    String[] numbersArray = numbers.split(",");
 	    if (numbersArray.length > 2) {
-	        throw new RuntimeException("Up to 2 numbers separated by comma (,) are allowed");
-	    } else {
-	        for (String number : numbersArray) {
-	            if (!number.isEmpty()) {
-	                Integer.parseInt(number);
-	            }
+	        throw new RuntimeException("Two Numbers separated by comma (,) are allowed");
+	    }
+	    for (String number : numbersArray) {
+	        if (!number.trim().isEmpty()) { // After refactoring
+	            returnValue += Integer.parseInt(number);
 	        }
 	    }
-	    return 0; // Added return
+	    return returnValue;
 	}
 }
