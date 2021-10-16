4024729decbf0647c18300a98ccdde97f13dfae4
Chetana Sonawane <chetanasonawane95@gmail.com>
2021-10-16 19:33:15 +0530

    Numbers bigger than 1000 should be ignored

--- StringCalculator/src/StringCalculator.java
+++ StringCalculator/src/StringCalculator.java
@@ -14,21 +14,22 @@ public class StringCalculator {
 	}
 	 
 	private static int add(final String numbers, final String delimiter) {
-	    int returnValue = 0;
-	    String[] numbersArray = numbers.split(delimiter);
-	    List negativeNumbers = new ArrayList();
-	    for (String number : numbersArray) {
-	        if (!number.trim().isEmpty()) {
-	            int numberInt = Integer.parseInt(number.trim());
-	            if (numberInt < 0) {
-	                negativeNumbers.add(numberInt);
-	            }
-	            returnValue += numberInt;
-	        }
-	    }
-	    if (negativeNumbers.size() > 0) {
-	        throw new RuntimeException("Negatives not allowed: " + negativeNumbers.toString());
-	    }
-	    return returnValue;     
-	}
+        int returnValue = 0;
+        String[] numbersArray = numbers.split(delimiter);
+        List negativeNumbers = new ArrayList();
+        for (String number : numbersArray) {
+                if (!number.trim().isEmpty()) {
+                        int numberInt = Integer.parseInt(number.trim());
+                        if (numberInt < 0) {
+                                negativeNumbers.add(numberInt);
+                        } else if (numberInt <= 1000) {
+                                returnValue += numberInt;
+                        }
+                }
+        }
+        if (negativeNumbers.size() > 0) {
+                throw new RuntimeException("Negatives not allowed: " + negativeNumbers.toString());
+        }
+        return returnValue;                
+}
 }
