b818ad6784cf155d15a5b70e6a677bf59f280208
Chetana Sonawane <chetanasonawane95@gmail.com>
2021-10-16 18:43:49 +0530

    Add Method to handle new lines between Numbers

--- StringCalculator/src/StringCalculatorTest.java
+++ StringCalculator/src/StringCalculatorTest.java
@@ -38,4 +38,8 @@ public class StringCalculatorTest
     public final void whenAnyNumberOfNumbersIsUsedThenReturnValuesAreTheirSums() {
         Assert.assertEquals(3+6+15+18+46+33, StringCalculator.add("3,6,15,18,46,33"));
     }
+    @Test
+    public final void whenNewLineIsUsedBetweenNumbersThenReturnValuesAreTheirSums() {
+        Assert.assertEquals(3+6+15, StringCalculator.add("3,6n15"));
+    }
 }
